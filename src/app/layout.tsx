42f1d214caf88231004d77b647c98f2c30e7c9e5
David Prelinger <53475909+David-Prelinger@users.noreply.github.com>
2023-09-19 22:43:15 -0600

    Added use client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -6,11 +8,13 @@ import { Theme } from 'react-daisyui'
 const inter = Inter({ subsets: ['latin'] })
 
 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <Theme>   <div className={inter.className}>
       {children}
